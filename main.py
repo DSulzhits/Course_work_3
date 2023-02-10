030498d218bbf629504a0e8a16430c4292543d8a
DSulzhits <dsulzhits@gmail.com>
2023-02-10 23:53:09 +0300

    создан файл main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from develop import utils
+from pprint import pprint
+
+
+def main():
+    OPERATIONS_URL = "https://www.jsonkeeper.com/b/VK9M"
+    num_of_operations = 5
+
+    data, info = utils.get_data(OPERATIONS_URL)
+    if not data:
+        exit(info)
+    else:
+        print(info)
+
+    operations_executed = utils.get_operations_executed(data)
+    last_five_operations = utils.get_last_five_operations(operations_executed, num_of_operations)
+    operations_formatted = utils.get_operations_formatted(last_five_operations)
+
+    for string in operations_formatted:
+        print(f"{string}\n")
+
+
+
+if __name__ == "__main__":
+    main()
